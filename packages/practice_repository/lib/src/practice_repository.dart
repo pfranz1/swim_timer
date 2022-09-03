4b640a9c3a442db3627df43ed089edc0cc54247a
pfranz1 <pfranz1@lsu.edu>
2022-09-03 14:23:29 -0500

    Add filtered streams to repository layer

--- packages/practice_repository/lib/src/practice_repository.dart
+++ packages/practice_repository/lib/src/practice_repository.dart
@@ -12,6 +12,48 @@ class PracticeRepository {
 
   Stream<List<Swimmer>> getSwimmers() => _practiceApi.getSwimmers();
 
+  /// Gets the swimmer who are on the deck
+  ///
+  /// Specifically, the swimmers in lane 0
+  Stream<List<Swimmer>> getDeckSwimmers() =>
+      // For every output from Api stream, apply a function to it then return
+      _practiceApi.getSwimmers().map((event) {
+        // Keep every element that meets criteria then return filtered
+        return event.where((element) {
+          // Return true, keeping element, if the lane is equal to 0
+          return element.lane == 0;
+        }).toList();
+      });
+
+  // The above function could also be written as using => notation
+  // Stream<List<Swimmer>> getDeckSwimmers() => _practiceApi.getSwimmers().map(
+  //       (event) => event.where((element) => element.lane == 0).toList(),
+  //     );
+
+  /// Gets the swimmers who are the blocks ready to swim next
+  ///
+  /// Specifically, the swimmers with a lane but with a null start time
+  Stream<List<Swimmer>> getBlockSwimmers() => _practiceApi.getSwimmers().map(
+        (event) => event
+            .where(
+                (element) => element.lane != null && element.startTime == null)
+            .toList(),
+      );
+
+  /// Gets the swimmers who are swimming in the pool
+  ///
+  /// Specifically, the swimers with a lane and a start time but no end time
+  // Maybe good to remove check for a lane, it should be enforced by the api
+  // but also maybe just to be safe leave it?
+  Stream<List<Swimmer>> getPoolSwimmers() => _practiceApi.getSwimmers().map(
+        (event) => event
+            .where((element) =>
+                element.lane != null &&
+                element.startTime != null &&
+                element.endTime == null)
+            .toList(),
+      );
+
   Future<void> addSwimmer(Swimmer swimmer) => _practiceApi.addSwimmer(swimmer);
 
   Future<void> removeSwimmer(String id) => _practiceApi.removeSwimmer(id);
