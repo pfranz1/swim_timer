da6e1f4ece640be60d3f31ddb7751fc9dc6ffa33
pfranz1 <pfranz1@lsu.edu>
2022-09-09 20:35:31 -0500

    add finisher entry object

---
+++ packages/practice_api/lib/src/finisher_entry.dart
@@ -0,0 +1,50 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:meta/meta.dart';
+
+part "finisher_entry.g.dart";
+
+// It is smelly to have stroke defined here and in swimmer
+// But I dont want to deal with json serializable issues and what not
+// The strokes wont change? Maybe could be expanded?
+// But later problem :D
+enum Stroke {
+  @JsonValue("free")
+  FREE_STYLE,
+  @JsonValue("back")
+  BACK_STROKE,
+  @JsonValue("breast")
+  BREAST_STROKE,
+  @JsonValue("fly")
+  BUTTERFLY,
+}
+
+@JsonSerializable()
+@immutable
+class FinisherEntry extends Equatable {
+  /// The name of the swimmer
+  final String name;
+
+  /// The id of the swimmer
+  final String id;
+
+  /// The time of the swimmer
+  final Duration time;
+  // The stroke the swimmer was swimming
+  final Stroke stroke;
+
+  FinisherEntry({
+    required this.id,
+    required this.name,
+    required this.time,
+    required this.stroke,
+  });
+
+  @override // For equatable
+  List<Object> get props => [stroke, id, name, time];
+
+  @override
+  String toString() {
+    return "{$name : $stroke : $time}";
+  }
+}
