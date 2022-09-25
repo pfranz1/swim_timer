9ec228ae5eb53670fdfb635d2484d0bb91fd8a22
pfranz1 <pfranz1@lsu.edu>
2022-09-24 21:55:34 -0500

    add back button to loading page

--- lib/pages/loading/loading_page.dart
+++ lib/pages/loading/loading_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:go_router/go_router.dart';
 
 /// As soon as this screen can it will call the provided function
 /// That function should probably have the size effect of navigating the user
@@ -25,6 +26,14 @@ class _LoadingPageState extends State<LoadingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Loading..."),
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.chevron_left),
+          onPressed: () => context.go('/'),
+        ),
+      ),
       body: Center(
         child: CircularProgressIndicator(),
       ),
