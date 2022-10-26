8325a9b398e195213fd6c7bf46be600f2312277c
aaronchenghs <aaronchenghs@gmail.com>
2022-10-26 12:36:50 -0500

    ID check for entities

--- packages/entities/lib/src/organization_coach.dart
+++ packages/entities/lib/src/organization_coach.dart
@@ -1,12 +1,17 @@
-// ignore_for_file: public_member_api_docs
+// ignore_for_file: public_member_api_docs, unnecessary_this
 import 'package:common/common.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:meta/meta.dart';
 
+@immutable
+@JsonSerializable()
 class OrgCoach {
   OrgCoach(
       {required String this.name,
       required String this.email,
-      required String this.password}) {
-    ID = Common.codeGenerator();
+      required String this.password,
+      String? id}) {
+    this.ID = id ?? Common.idGenerator();
   }
   final String name;
   final String email;
